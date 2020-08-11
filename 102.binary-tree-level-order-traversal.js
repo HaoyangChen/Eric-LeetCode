e496028f16362d3b16e73cafe57e928085d9f458
Haoyang Chen <haoyangchen0901@gmail.com>
2020-08-11 00:08:20 -0700

    Completed 102, 107, 88

---
+++ 102.binary-tree-level-order-traversal.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=102 lang=javascript
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+ // Method: Breadth First Search (BFS)
+ // Time Complexity: O(n) - Each node gets in and gets out of the queue once
+ // Space Complexity: O(n) - The maximum number of elements in the queue doesn't exceed n
+var levelOrder = function(root) {
+    const ret = [];
+    if (!root) return ret;
+    const q = [];
+    q.push(root);
+    while (q.length !== 0) {
+        const currentLevelSize = q.length;
+        ret.push([]);
+        for (let i = 1; i <= currentLevelSize; i++) {
+            const node = q.shift();
+            ret[ret.length - 1].push(node.val);
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right)
+        }
+    }
+    return ret;
+};
+// @lc code=end
+
---
+++ 107.binary-tree-level-order-traversal-ii.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=107 lang=javascript
+ *
+ * [107] Binary Tree Level Order Traversal II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+ // This is the reversed version of number 102 Binary tree level order traversal
+// Time Complexity: O(n) - Each node gets in and gets out of the queue once
+ // Space Complexity: O(n) - The maximum number of elements in the queue doesn't exceed n
+var levelOrderBottom = function(root) {
+    const ret = [];
+    if (!root) return ret;
+    const q = [];
+    q.push(root);
+    while (q.length !== 0) {
+        const currentLevelSize = q.length;
+        ret.push([]);
+        for (let i = 1; i <= currentLevelSize; i++) {
+            const node = q.shift();
+            ret[ret.length - 1].push(node.val);
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right)
+        }
+    }
+    return ret.reverse();
+};
+// @lc code=end
+
