e82eb6495799e759f6a6773f4e25007bc62a83fe
Haoyang Chen <haoyangchen0901@gmail.com>
2020-08-23 23:34:44 -0700

    Completed 203

---
+++ 203.remove-linked-list-elements.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=203 lang=javascript
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+var removeElements = function (head, val) {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+
+  let prev = dummy,
+    curr = head;
+  while (curr !== null) {
+    if (curr.val === val) {
+      prev.next = curr.next;
+    } else {
+      prev = curr;
+    }
+    curr = curr.next;
+  }
+  return dummy.next;
+};
+// @lc code=end
