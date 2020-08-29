018462d4277557ab3f51e032180ab77c8afb1633
Haoyang Chen <haoyangchen0901@gmail.com>
2020-08-28 23:10:14 -0700

    Completed 198

--- 198.house-robber.js
+++ 198.house-robber.js
@@ -9,8 +9,46 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    
-};
-// @lc code=end
 
+// Method: Dynamic Programming
+// Time Complexity: O(n) - where n is the length of the array. We only need to traverse the array once
+// Space Complexity: O(n) - extra space with length of the given array is required
+// var rob = function (nums) {
+//   if (nums === null || nums.length === 0) {
+//     return 0;
+//   }
+//   let length = nums.length;
+//   if (length === 1) {
+//     return nums[0];
+//   }
+//   let dp = new Array(length);
+//   dp[0] = nums[0];
+//   dp[1] = Math.max(nums[0], nums[1]);
+//   for (let i = 2; i < length; i++) {
+//     dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+//   }
+//   return dp[length - 1];
+// };
+
+// Method: Dynamic Programming with improvement on space complexity
+// Time Complexity: O(n) - where n is the length of the array. We only need to traverse the array once
+// Space Complexity: O(1) - With scrolling arrays, it allows to store the maximum amount of money in the previous two hourses, rather than storing the results of the entire array. So the space complexity is O(1).
+var rob = function (nums) {
+  if (nums === null || nums.length === 0) {
+    return 0;
+  }
+  let length = nums.length;
+  if (length === 1) {
+    return nums[0];
+  }
+  let first = nums[0],
+    second = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < length; i++) {
+    let temp = second;
+    second = Math.max(first + nums[i], second);
+    first = temp;
+  }
+  return second;
+};
+
+// @lc code=end
