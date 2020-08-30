15467d4f95924cf429e649b5fbe9d9d1d2ee20e0
Haoyang Chen <haoyangchen0901@gmail.com>
2020-08-30 13:15:50 -0700

    Completed 257

--- 257.binary-tree-paths.js
+++ 257.binary-tree-paths.js
@@ -17,8 +17,27 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
-var binaryTreePaths = function(root) {
-    
+
+// Method: Recursion
+// Time Complexity: O(n) - Each node in the tree is visited exactly once
+// Space Complexity: O(n) - In the worst case, when the binary tree only has one child node for each of its node, the level of recursion is n and the space complexity is O(n).
+//                          In the best case, when the binary tree is balanced, its height is O(logn). The time complexity is O(log(n))
+var binaryTreePaths = function (root) {
+  let paths = [];
+  constructPath(root, "", paths);
+  return paths;
+};
+
+const constructPath = (root, path, paths) => {
+  if (root !== null) {
+    path += root.val;
+    if (root.left === null && root.right === null) {
+      paths.push(path);
+    } else {
+      path += "->";
+      constructPath(root.left, path, paths);
+      constructPath(root.right, path, paths);
+    }
+  }
 };
 // @lc code=end
-
