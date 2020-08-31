3eae3debd69b2cea1a789267254ebb95b51ef822
Haoyang Chen <haoyangchen0901@gmail.com>
2020-08-30 17:03:51 -0700

    Completed 257

--- 257.binary-tree-paths.js
+++ 257.binary-tree-paths.js
@@ -22,22 +22,52 @@
 // Time Complexity: O(n) - Each node in the tree is visited exactly once
 // Space Complexity: O(n) - In the worst case, when the binary tree only has one child node for each of its node, the level of recursion is n and the space complexity is O(n).
 //                          In the best case, when the binary tree is balanced, its height is O(logn). The time complexity is O(log(n))
+// var binaryTreePaths = function (root) {
+//   let paths = [];
+//   constructPath(root, "", paths);
+//   return paths;
+// };
+
+// const constructPath = (root, path, paths) => {
+//   if (root !== null) {
+//     path += root.val;
+//     if (root.left === null && root.right === null) {
+//       paths.push(path);
+//     } else {
+//       path += "->";
+//       constructPath(root.left, path, paths);
+//       constructPath(root.right, path, paths);
+//     }
+//   }
+// };
+
+// Method2: Iterative
+// Time Complexity: O(n) - Each node is visited exactly once
+// Space Complexity: O(n) - In the worst case, the queue contains n nodes
 var binaryTreePaths = function (root) {
   let paths = [];
-  constructPath(root, "", paths);
-  return paths;
-};
-
-const constructPath = (root, path, paths) => {
-  if (root !== null) {
-    path += root.val;
-    if (root.left === null && root.right === null) {
+  if (root === null) {
+    return paths;
+  }
+  let nodeStack = [];
+  let pathStack = [];
+  nodeStack.push(root);
+  pathStack.push(root.val.toString());
+  while (nodeStack.length !== 0) {
+    let node = nodeStack.pop();
+    let path = pathStack.pop();
+    if (node.left === null && node.right === null) {
       paths.push(path);
-    } else {
-      path += "->";
-      constructPath(root.left, path, paths);
-      constructPath(root.right, path, paths);
+    }
+    if (node.left !== null) {
+      nodeStack.push(node.left);
+      pathStack.push(path + "->" + node.left.val);
+    }
+    if (node.right !== null) {
+      nodeStack.push(node.right);
+      pathStack.push(path + "->" + node.right.val);
     }
   }
+  return paths;
 };
 // @lc code=end
