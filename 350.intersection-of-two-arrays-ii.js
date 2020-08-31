5322e625c0da2c4fc83364bbe24e8250571c8180
Haoyang Chen <haoyangchen0901@gmail.com>
2020-08-30 17:34:56 -0700

    Completed 350

---
+++ 350.intersection-of-two-arrays-ii.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=350 lang=javascript
+ *
+ * [350] Intersection of Two Arrays II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+// Method 1: Hashmap
+// Time Complexity: O(m + n) - where m and n are the length of the given two arrays. We need to traverse both arrays and manipulate the hashmap. The time complexity of manipulating hashmap is O(1), so the overall time complexity is related to the length of the two given arrays.
+// Space Complexity: O(min(m, n)) - The size of the hashmap is at most the length of the given array with smaller size.
+var intersect = function (nums1, nums2) {
+  let hashmap = new Map();
+  if (nums1.length > nums2.length) {
+    intersect(nums2, nums1);
+  }
+
+  for (let i = 0; i < nums1.length; i++) {
+    let value = nums1[i];
+    let count = hashmap.has(value) ? hashmap.get(value) + 1 : 1;
+    hashmap.set(value, count);
+  }
+
+  let intersection = [];
+  let index = 0;
+  for (let i = 0; i < nums2.length; i++) {
+    let num = nums2[i];
+    let count = hashmap.has(num) ? hashmap.get(num) : 0;
+    if (count > 0) {
+      intersection[index++] = num;
+      count--;
+      if (count > 0) {
+        hashmap.set(num, count);
+      } else {
+        hashmap.delete(num);
+      }
+    }
+  }
+  return intersection;
+};
+// @lc code=end
