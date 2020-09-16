94258df620c1c756006a6735ac7a0d7828731b70
Haoyang Chen <haoyangchen0901@gmail.com>
2020-09-16 01:21:32 -0700

    Completed 39

--- 39.combination-sum.js
+++ 39.combination-sum.js
@@ -10,8 +10,27 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
-    
+
+// Time Complexity: O(S) - S is the The sum of the lengths of all the possible solutions
+// Space Complexity: O(target) - the space complexity depends on the recursion stack. In the worst case, we need to cover O(target) levels of recursion
+var combinationSum = function (candidates, target) {
+  const ans = [];
+  const dfs = (target, combine, idx) => {
+    if (idx === candidates.length) {
+      return;
+    }
+    if (target === 0) {
+      ans.push(combine);
+      return;
+    }
+    // skip
+    dfs(target, combine, idx + 1);
+    // select the current number
+    if (target - candidates[idx] >= 0) {
+      dfs(target - candidates[idx], [...combine, candidates[idx]], idx);
+    }
+  };
+  dfs(target, [], 0);
+  return ans;
 };
 // @lc code=end
-
