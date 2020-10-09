2c988d62d6873bb990a83eea0d5a12c6485b326a
Haoyang Chen <haoyangchen0901@gmail.com>
2020-10-09 00:27:47 -0700

    Completed 448

--- 448.find-all-numbers-disappeared-in-an-array.js
+++ 448.find-all-numbers-disappeared-in-an-array.js
@@ -9,8 +9,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDisappearedNumbers = function(nums) {
-    
+var findDisappearedNumbers = function (nums) {
+  // Hash table for keeping track of the numbers in the array
+  let hashtable = new Map();
+
+  // Add each of the numbers to the hash table
+  for (let i = 0; i < nums.length; i++) {
+    hashtable.set(nums[i], true);
+  }
+
+  // Response array that would contain the missing number
+  let result = [];
+
+  // Iterate over the numbers from 1 to N and add all those that don't appear in the hash table
+  for (let i = 1; i <= nums.length; i++) {
+    if (!hashtable.has(i)) {
+      result.push(i);
+    }
+  }
+  return result;
 };
 // @lc code=end
-
