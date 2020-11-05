4da278795a06443393b36866f16e5a2fe6ae9772
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-04 23:20:27 -0800

    Update 653

--- 653.two-sum-iv-input-is-a-bst.js
+++ 653.two-sum-iv-input-is-a-bst.js
@@ -22,16 +22,44 @@
 // Using Hashset
 // Time Complexity: O(n) - The entire tree is traversed only once in the worst case. Here, n refers to the number of nodes in the given tree.
 // Space Complexity: O(n)  - The size of the set can grow up to n in the worst case.
+// var findTarget = function (root, k) {
+//   const set = new Set();
+//   return find(root, k, set);
+// };
+
+// const find = (root, k, set) => {
+//   if (root === null) return false;
+//   if (set.has(k - root.val)) return true;
+//   set.add(root.val);
+//   return find(root.left, k, set) || find(root.right, k, set);
+// };
+
+// Using BFS and Hashset
+// Algorithm:
+// 1: Remove an element p from the front of the queue
+// 2: Check if the element k - p already exists in the set. If so, return True
+// 3: Otherwise, add this element p to the set. Further, add the right and left child nodes of the current node to the back of the queue
+// 4: Continue step 1 to 3 till the queue becomes empty
+// 5: Return false if the queue becomes empty
+
+// Time Complexity: O(n). We need to traverse the whole tree once in the worst case. Here, n refers to the number of nodes in the given tree.
+// Space Complexity: O(n). The size of the set can grow almost up to n.
 var findTarget = function (root, k) {
   const set = new Set();
-  return find(root, k, set);
-};
-
-const find = (root, k, set) => {
-  if (root === null) return false;
-  if (set.has(k - root.val)) return true;
-  set.add(root.val);
-  return find(root.left, k, set) || find(root.right, k, set);
+  const queue = [];
+  queue.push(root);
+  while (queue.length !== 0) {
+    if (queue[0] !== null) {
+      const node = queue.shift();
+      if (set.has(k - node.val)) return true;
+      set.add(node.val);
+      queue.push(node.right);
+      queue.push(node.left);
+    } else {
+      queue.shift();
+    }
+  }
+  return false;
 };
 
 // @lc code=end
