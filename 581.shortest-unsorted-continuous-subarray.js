2d18edfd4aa0588cc96ffdde93a4a481f3314f4e
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-12 23:51:17 -0800

    Complete 581

--- 581.shortest-unsorted-continuous-subarray.js
+++ 581.shortest-unsorted-continuous-subarray.js
@@ -9,8 +9,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findUnsortedSubarray = function(nums) {
-    
+var findUnsortedSubarray = function (nums) {
+  let l = nums.length,
+    r = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[i]) {
+        r = Math.max(r, j);
+        l = Math.min(l, i);
+      }
+    }
+  }
+  return r - l < 0 ? 0 : r - l + 1;
 };
 // @lc code=end
-
