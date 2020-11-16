5295d5e1794c522c26a2908285815ec66727f4e0
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-15 23:44:38 -0800

    Complete 209

--- 209.minimum-size-subarray-sum.js
+++ 209.minimum-size-subarray-sum.js
@@ -10,8 +10,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minSubArrayLen = function(s, nums) {
-    
+var minSubArrayLen = function (s, nums) {
+  let n = nums.length;
+  let ans = Number.MAX_SAFE_INTEGER;
+  let left = 0;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    while (sum >= s) {
+      ans = Math.min(ans, i + 1 - left);
+      sum -= nums[left++];
+    }
+  }
+  return ans !== Number.MAX_SAFE_INTEGER ? ans : 0;
 };
 // @lc code=end
-
