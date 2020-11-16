df451f2fb70a066813522fdb9dcb60078c265ca7
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-15 23:51:14 -0800

    Add Complexity Analysis for 209

--- 209.minimum-size-subarray-sum.js
+++ 209.minimum-size-subarray-sum.js
@@ -10,12 +10,22 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// Method: 2 pointers approach
+// Algorithm:
+// 1: Initialize left pointer to 0 and sum to 0
+// 2: Iterate over the nums: add nums[i] to sum
+// 3: While sum is greater than or equal to s: update ans = Math.min(ans, i + 1 - left), where i + 1- left is the size of the current subarray
+//    Subtract nums[left] from sum and increment left
+
+// Time Complexity: O(n) - single iteration of O(n) --> Each letter can be visited at most twice, once by the right pointer and at most once by the left pointer.
+// Space Complexity: O(1) extra space. Only constant space required for left, sum, ans, and i
 var minSubArrayLen = function (s, nums) {
   let n = nums.length;
   let ans = Number.MAX_SAFE_INTEGER;
   let left = 0;
   let sum = 0;
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < n; i++) {
     sum += nums[i];
     while (sum >= s) {
       ans = Math.min(ans, i + 1 - left);
