f438d55a81ce6698554c8f1981f70295641ad0fb
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-20 23:39:12 -0800

    Complete 63

--- 63.unique-paths-ii.js
+++ 63.unique-paths-ii.js
@@ -10,22 +10,28 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function (obstacleGrid) {
-  let n = obstacleGrid.length,
-    m = obstacleGrid[0].length;
-  let f = new Array(m);
-
-  f[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
-  for (let i = 0; i < n; ++i) {
-    for (let j = 0; j < m; ++j) {
-      if (obstacleGrid[i][j] == 1) {
-        f[j] = 0;
-        continue;
-      }
-      if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
-        f[j] += f[j - 1];
-      }
+  if (obstacleGrid[0][0] == 1) return 0; // 出发点就被障碍堵住
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  // dp数组初始化
+  const dp = new Array(m);
+  for (let i = 0; i < m; i++) dp[i] = new Array(n);
+  // base case
+  dp[0][0] = 1; // 终点就是出发点
+  for (let i = 1; i < m; i++) {
+    // 第一列其余的case
+    dp[i][0] = obstacleGrid[i][0] == 1 || dp[i - 1][0] == 0 ? 0 : 1;
+  }
+  for (let i = 1; i < n; i++) {
+    // 第一行其余的case
+    dp[0][i] = obstacleGrid[0][i] == 1 || dp[0][i - 1] == 0 ? 0 : 1;
+  }
+  // 迭代
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = obstacleGrid[i][j] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
     }
   }
-  return f[m - 1];
+  return dp[m - 1][n - 1]; // 到达(m-1,n-1)的路径数
 };
 // @lc code=end
