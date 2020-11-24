51d9767675e0d8e17ff3b18735ae88d6a073119e
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-23 23:56:35 -0800

    Add Second Method for Problem 485

--- 485.max-consecutive-ones.js
+++ 485.max-consecutive-ones.js
@@ -9,12 +9,23 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var findMaxConsecutiveOnes = function (nums) {
+//   return Math.max(
+//     ...nums
+//       .join("")
+//       .split("0")
+//       .map((ones) => ones.length)
+//   );
+// };
+
 var findMaxConsecutiveOnes = function (nums) {
-  return Math.max(
-    ...nums
-      .join("")
-      .split("0")
-      .map((ones) => ones.length)
-  );
+  let max = 0,
+    curr = 0;
+  for (let k of nums) {
+    max = Math.max(max, (curr += k));
+    if (!k) curr = 0;
+  }
+  return max;
 };
+
 // @lc code=end
