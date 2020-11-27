f2c6318578221628c7fe82eadc3004f755ae5cb2
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-26 23:46:20 -0800

    Add Question 566

---
+++ 566.reshape-the-matrix.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=566 lang=javascript
+ *
+ * [566] Reshape the Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} nums
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function(nums, r, c) {
+    
+};
+// @lc code=end
+
