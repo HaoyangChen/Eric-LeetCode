2b1d6d4fe8c1ded5e43310733c288a7cb1de0c05
Haoyang Chen <haoyangchen0901@gmail.com>
2020-11-29 23:39:58 -0800

    Complete 268

--- 268.missing-number.js
+++ 268.missing-number.js
@@ -9,8 +9,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-    
+var missingNumber = function (nums) {
+  let numSet = new Set();
+  for (let num of nums) {
+    numSet.add(num);
+  }
+  const expectedNumCount = nums.length + 1;
+  for (let number = 0; number < expectedNumCount; number++) {
+    if (!numSet.has(number)) {
+      return number;
+    }
+  }
+  return -1;
 };
 // @lc code=end
-
