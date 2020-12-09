3062c41f4a3a355241d0fa997c7526fb91bae2b4
Haoyang Chen <haoyangchen0901@gmail.com>
2020-12-08 23:47:39 -0800

    Complete Problem 258

---
+++ 1530.number-of-good-leaf-nodes-pairs.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1530 lang=javascript
+ *
+ * [1530] Number of Good Leaf Nodes Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} distance
+ * @return {number}
+ */
+var countPairs = function(root, distance) {
+    
+};
+// @lc code=end
+
---
+++ 258.add-digits.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=258 lang=javascript
+ *
+ * [258] Add Digits
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+  return ((num - 1) % 9) + 1;
+};
+// @lc code=end
