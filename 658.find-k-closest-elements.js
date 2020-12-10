33d8687f37499b4656363848411bee4d43300a16
Haoyang Chen <haoyangchen0901@gmail.com>
2020-12-09 23:58:48 -0800

    Start Problem 658

---
+++ 658.find-k-closest-elements.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=658 lang=javascript
+ *
+ * [658] Find K Closest Elements
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+    
+};
+// @lc code=end
+
